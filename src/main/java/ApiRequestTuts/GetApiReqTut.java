dd72e71c64d9bba6953fe999b60bf9773375f529
vedant202 <vdhenge202@gmail.com>
2023-08-04 11:59:48 +0530

    String list Json data to Object using Gson library

--- src/main/java/ApiRequestTuts/GetApiReqTut.java
+++ src/main/java/ApiRequestTuts/GetApiReqTut.java
@@ -11,14 +11,80 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import com.google.gson.Gson; 
+import com.google.gson.GsonBuilder;
+import java.util.ArrayList;
+import java.util.List;
+ 
 
 /**
  *
  * @author DELL
  */
+class NewsObjList{
+    List<NewsObj> newsObjList;
 
+    @Override
+    public String toString() {
+        return "NewsObjList{" + "newsObjList=" + newsObjList + '}';
+    }
+    
+    
+}
+
+class NewsObj{
+    int id;
+    int userId;
+    String title;
+    String body;
+    
+    public NewsObj(){
+       
+    }
+
+    @Override
+    public String toString() {
+        return "NewsObj{" + "id=" + id + ", userId=" + userId + ", title=" + title + ", body=" + body + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+   
+}
 class GetData{
     private String url = "";
+    private String resp = "";
+    private List<NewsObj> jsonResp;
     
     public void seturl(String url){
         this.url = url;
@@ -28,7 +94,7 @@ class GetData{
         return this.url;
     }
     
-    public void getJsonData(){
+    public void getStringData(){
         
         
         HttpRequest request = HttpRequest.newBuilder().GET()
@@ -47,9 +113,35 @@ class GetData{
         
         System.out.println(response.statusCode());
         System.out.println("Type of Response "+response.body().getClass().getName());
-        System.out.println(response.body());
+        //System.out.println(response.body());
+        this.resp = response.body();
         
         
+    }
+    
+    public void getJsonData(){
+       
+        GsonBuilder builder= new GsonBuilder();
+        builder.setPrettyPrinting();
+        
+         
+        Gson gSon = builder.create();
+        List<NewsObj> newsObjList = new ArrayList<>();
+//          System.out.println(this.resp);
+        List<NewsObj> json = gSon.fromJson(this.resp, newsObjList.getClass());
+        this.jsonResp = json;
+        System.out.println(json);
+        System.out.println(json.getClass().getName());
+        
+
+          
+    }
+    public void getTitle(){
+//        System.out.println(this.jsonResp);
+        
+//        for(var i:this.jsonResp){
+//            System.out.println(i);
+//        }
     }
     
 }
@@ -59,7 +151,9 @@ public class GetApiReqTut {
         GetData data = new GetData();
         data.seturl(url);
         
+        data.getStringData();
         data.getJsonData();
+        data.getTitle();
         
     }
 }
